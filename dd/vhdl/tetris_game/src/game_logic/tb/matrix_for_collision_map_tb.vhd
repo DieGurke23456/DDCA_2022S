1fdd97858f8435ad7fa89d6f00fe5323933261f5
Ismail Yilmaz <isi.2000@hotmail.com>
2022-04-30 15:40:38 +0200

    tb

--- dd/vhdl/tetris_game/src/game_logic/tb/matrix_for_collision_map_tb.vhd
+++ dd/vhdl/tetris_game/src/game_logic/tb/matrix_for_collision_map_tb.vhd
@@ -91,7 +91,7 @@ begin
         check_solid :process(all)
         begin 
             echo ("checking block " & integer'image(block_map_x_int) & " " & integer'image(block_map_y_int) & LF); 
-            if (block_map_y_int > 0 and block_map_y_int < test_matrix'length and block_map_x_int > 0 and block_map_x_int < BLOCKS_X) then 
+            if (block_map_y_int => 0 and block_map_y_int < test_matrix'length and block_map_x_int => 0 and block_map_x_int < BLOCKS_X) then 
                 if (test_matrix(block_map_y_int)(block_map_x_int) = T_BB_EMPTY) then 
                     tc_block_map_solid <= '0';
                 else 
