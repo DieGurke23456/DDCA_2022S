76af313923766508ce63a60265efade38c6cef83
Ismail Yilmaz <isi.2000@hotmail.com>
2022-04-30 12:54:21 +0200

    test-cases

--- dd/vhdl/tetris_game/src/game_logic/tb/tetris_game_logic_pkg_tb.vhd
+++ dd/vhdl/tetris_game/src/game_logic/tb/tetris_game_logic_pkg_tb.vhd
@@ -44,42 +44,43 @@ begin
     main : process
         begin
         
-        report "1 :is_solid_at(row)";
+        --report "1 :is_solid_at(row)";
         assert is_solid_at(TEST_MATRIX_A(0), 0) = '1' report "test 1 failed!" severity error; 
         
-        report "2 :is_solid_at(row)";
+        --report "2 :is_solid_at(row)";
         assert is_solid_at(TEST_MATRIX_A(1), 0) = '0' report "test 2 failed!" severity error;
         
-        report "3 :is_solid_at(matrix)";
+        --report "3 :is_solid_at(matrix)";
         assert is_solid_at(TEST_MATRIX_A, 0, 0) = '1' report "test 3 failed!" severity error;
 
-        report "4 :is_solid_at(matrix)";
+        --report "4 :is_solid_at(matrix)";
         assert is_solid_at(TEST_MATRIX_A, 0,1) = '0' report "test 4 failed!" severity error;
 
-        report "5 :equals(rows)";
+        --report "5 :equals(rows)";
         assert equals(OUT_MATRIX_A(0), OUT_MATRIX_A(0)) report "test 5 failed!" severity error;
 
-        report "6 :equals(rows)";
+        --report "6 :equals(rows)";
         assert not equals(OUT_MATRIX_A(0), OUT_MATRIX_A(1)) report "test 6 failed!" severity error;
 
-        report "7 :row_to_solidity_vector";
+        --report "7 :row_to_solidity_vector";
         assert equals(row_to_solidity_vector(TEST_MATRIX_A(0)), OUT_MATRIX_A(0)) report "test 7 failed!" severity error;
         
-        report "8 :row_to_solidity_vector";
+        --report "8 :row_to_solidity_vector";
         assert equals(row_to_solidity_vector(TEST_MATRIX_A(1)), OUT_MATRIX_A(1)) report "test 8 failed!" severity error;
         
-        report "9 :equals(matrixs)";
+        --report "9 :equals(matrixs)";
         assert equals(OUT_MATRIX_A, OUT_MATRIX_A) report "test 9 failed!" severity error;
 
-        report "10 :equals(matrixs)";
+        --report "10 :equals(matrixs)";
         assert not equals(OUT_MATRIX_A, OUT_MATRIX_B) report "test 10 failed!" severity error;
         
-        report "11 :matrix_to_solidity_matrix";
+        --report "11 :matrix_to_solidity_matrix";
         assert equals(matrix_to_solidity_matrix(TEST_MATRIX_A), OUT_MATRIX_A) report "test 11 failed!" severity error;
 
-        report "12 :matrix_to_solidity_matrix";
+        --report "12 :matrix_to_solidity_matrix";
         assert equals(matrix_to_solidity_matrix(TEST_MATRIX_B), OUT_MATRIX_B) report "test 12 failed!" severity error;        
         
+        report "test finished";
         wait;
 
     end process;    
